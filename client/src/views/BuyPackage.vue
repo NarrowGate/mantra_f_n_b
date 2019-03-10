48f196f204e6ed475dad010bb2e48c209e2d9e7f
NarrowGate <melvinwilson2012@gmail.com>
2019-03-10 17:41:37 +1000

    implement and test paypal component

--- client/src/views/BuyPackage.vue
+++ client/src/views/BuyPackage.vue
@@ -104,15 +104,24 @@
                         </div>
                     </div>
                     <div class="columns">
-                        <div class="column">
+                        <div class="column is-6">
                             <div class="control">
                                 <!-- <paypal
-                                    amount="10.00"
-                                    currency="USD"
-                                    :client="credentials">
+                                    amount="1.00"
+                                    currency="AUD"
+                                    :client="credentials"
+                                    :env="sandbox"
+                                    :button-style="myStyle"
+                                    @payment-authorized="payment_authorized_cb"
+                                    @payment-completed="payment_completed_cb"
+                                    @payment-cancelled="payment_cancelled_cb"
+                                    >
                                 </paypal> -->
-                                <button class="button is-primary" @click.prevent = "payWithPaypal">Pay with Paypal</button>
+                                <!-- <button class="button is-primary" @click.prevent = "payWithPaypal">Pay with Paypal</button> -->
                             </div>                      
+                        </div>
+                        <div class="column is-6">
+                            <button class="button is-primary" @click.prevent = "payWithPaypal">Pay with Paypal</button>
                         </div>
                     </div>                                                     
                 </form>
@@ -137,18 +146,25 @@ import { required, email, numeric } from 'vuelidate/lib/validators';
 export default {
     data () {
         return {
-            package: this.$route.params.id,
+            packageFromRoute: this.$route.params.id,
             credentials: {
-                sandbox:'AQjF9Nvyc-W5MNcGIuXDkXAYBu_-2Rxyhxxx-jreQlFaVHwLgDbQxz8wgFnUhcgtdYTnhEOlYE9a6xfa',
-                production:'Aadxfwml9vmGU8QXOQERb_mkuQ8OnYNKaPG9O30Px8VuSIyOTUEHQqDV1nqshCaI879I84coOlIGGee7'
+                sandbox:'AVP6sBXr3Zhf0hT3Vczssi19Mqe6hmN3pbtDfYID_OTR9INGD7_AQ4ytmKgk-g3l2P8v71FsFcC8h9_O',
+                production:'ATKAkG91Z5OUK4nduGVB8wIBkYuZx7o0yjn2MD_6O55UlLGpyGKIcpPQm-FQYAxiXIhmsU-azxyAGnC1'
             },
-            firstName:'',
-            lastName:'',
-            contactNumber:'',
-            email:'',
-            pickupAddress:'',
-            transmission:'',
-            package:''
+            firstName:'Selvam',
+            lastName:'Sundar',
+            contactNumber:6878,
+            email:'reshmaclarance@gmail.com',
+            pickupAddress:'12 Kornuta Close, Bellowrie',
+            transmission:'Automatic',
+            package:'Package B',
+            packagePrice:55,
+            myStyle: {                
+                label: '',
+                size:  'responsive',
+                shape: 'rect',
+                color: 'black'                
+            }
         }
     },
     components: {
@@ -173,23 +189,27 @@ export default {
     methods: {
         payWithPaypal : () => {
 
-        const transactionDetails = {
-                'product' :'product1'
+            const userDetails = {
+                first_name: this.firstName,
+                second_name: this.lastName,
+                contact_number: this.contactNumber,
+                email: this.email,
+                pickup_address:this.pickupAddress,
+                transmission: this.transmission,
+                purchasedPackage: this.package,
+                purchasedPackagePrice:packagePrice
             }
+
             console.log('hit paypal button');
 
-
-            axios.post('/buywithpaypal',transactionDetails)
+            axios.post('/buywithpaypal',userDetails)
             .then((dfs)=> {
                 console.log('axios post sent')
-                console.log(dfs.data);
-
-                
+                console.log(dfs.data);                
             })
             .catch((err) => {
                 console.log(err)
             })
-
 
             // axios.get('/buywithpaypal')
             // .then((dfs)=> {
@@ -199,7 +219,44 @@ export default {
             //     console.log(err)
             // })
 
+        },
+
+        payment_authorized_cb : () => {
+
+        },
+
+        payment_completed_cb : (data) => {
+
+        console.log(data);
+
+        // Paypal account details (not necessarily the user)
+        // let payerAccountDetails = {
+        //     first_name : data.payer.payer_info.first_name,
+        //     last_name : data.payer.payer_info.last_name,
+        //     payer_id: data.payer.payer_info.payer_id,
+        //     payment_method: data.payer.payment_method,
+        //     status: data.state,
+        //     total: data.transactions[0].amount.total,
+        //     currency:data.payer.payer_info.country_code
+        // }
+
+        // Sample data
+        let payerAccountDetails = {
+            first_name : 'Reshma',
+            last_name : 'Melvin',
+            payer_id: 'some id',
+            payment_method: 'Paypal Method',
+            status: 'Approved',
+            total: 10,
+            currency:'AUD'
         }
+
+
+        },
+
+        payment_cancelled_cb : () => {
+
+        },
     }
 }
 </script>
